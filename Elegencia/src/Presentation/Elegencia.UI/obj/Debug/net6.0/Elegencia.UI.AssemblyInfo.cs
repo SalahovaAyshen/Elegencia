7f57fda8609d2a7f9ed50b1442f1116e61fa8c35
ayshen <TU81UEBFJ@code.edu.az>
2024-02-23 14:43:29 +0400

    added global exception

--- Elegencia/src/Presentation/Elegencia.UI/obj/Debug/net6.0/Elegencia.UI.AssemblyInfo.cs
+++ Elegencia/src/Presentation/Elegencia.UI/obj/Debug/net6.0/Elegencia.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Elegencia.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71df449a35cb4b839e7b7abc812339e8ce069026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28310e209db7b262d07b0c40316da461a3245d90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Elegencia.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Elegencia.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
